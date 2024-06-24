1b81a8161f513342b8e346a51a9d3c6421cb6369
Senura Perera <senura.perera@arimaclanka.com>
2024-06-24 15:53:22 +0530

    Add settings section

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,8 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use A17\Twill\Facades\TwillNavigation;
 use A17\Twill\View\Components\Navigation\NavigationLink;
+use A17\Twill\Facades\TwillAppSettings;
+use A17\Twill\Services\Settings\SettingsGroup;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -27,6 +29,8 @@ class AppServiceProvider extends ServiceProvider
         TwillNavigation::addLink(
             NavigationLink::make()->forModule('menuLinks')
         );
-
+        TwillAppSettings::registerSettingsGroup(
+            SettingsGroup::make()->name('homepage')->label('Homepage')
+        );
     }
 }
